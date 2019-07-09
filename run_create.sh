487e0666b2ed361c3df0f4bf241cdd604d83c2ce
Auro Tripathy <ipserv@yahoo.com>
2019-07-09 05:40:47 +0000

    initial

---
+++ run_create.sh
@@ -0,0 +1,11 @@
+export BERT_BASE_DIR=uncased_L-12_H-768_A-12
+python3 create_pretraining_data.py \
+       --input_file=./sample_text.txt \
+       --output_file=/tmp/tf_examples.tfrecord \
+       --vocab_file=$BERT_BASE_DIR/vocab.txt \
+       --do_lower_case=True \
+       --max_seq_length=128 \
+       --max_predictions_per_seq=20 \
+       --masked_lm_prob=0.15 \
+       --random_seed=12345 \
+       --dupe_factor=5
