babbeb742c0ce918fc6bc3e69d493cc84e5d05e3
Auro Tripathy <ipserv@yahoo.com>
2019-07-11 01:18:49 +0000

    comments

--- run_classify.sh
+++ run_classify.sh
@@ -1,14 +1,15 @@
+# cola is Corpus of Linguistic Acceptability
 export BERT_BASE_DIR=uncased_L-12_H-768_A-12
 python3 run_classifier_multi_gpu.py \
-       --task_name=cola \
-       --do_train=true \
-       --do_eval=true \
-       --data_dir=./data \
-       --vocab_file=$BERT_BASE_DIR/vocab.txt \
-       --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-       --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-       --max_seq_length=128 \
-       --train_batch_size=32 \
-       --learning_rate=2e-5 \
-       --num_train_epochs=3.0 \
-       --output_dir=./bert_output/
+	--task_name=cola \
+	--do_train=true \
+	--do_eval=true \
+	--data_dir=./data \
+	--vocab_file=$BERT_BASE_DIR/vocab.txt \
+	--bert_config_file=$BERT_BASE_DIR/bert_config.json \
+	--init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+	--max_seq_length=128 \
+	--train_batch_size=32 \
+	--learning_rate=2e-5 \
+	--num_train_epochs=3.0 \
+	--output_dir=./bert_output/
