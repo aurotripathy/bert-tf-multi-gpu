ddf74cb5be253eecc0847f151be5ede6cdc4f84c
Auro Tripathy <ipserv@yahoo.com>
2019-07-12 20:23:57 +0000

    removed init'ing from checkpoint

--- run_pre.sh
+++ run_pre.sh
@@ -1,3 +1,4 @@
+#       --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
 export BERT_BASE_DIR=uncased_L-12_H-768_A-12
 python3 run_pretraining_multi_gpu.py \
        --input_file=/tmp/tf_examples.tfrecord \
@@ -5,7 +6,6 @@ python3 run_pretraining_multi_gpu.py \
        --do_train=True \
        --do_eval=True \
        --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-       --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
        --train_batch_size=32 \
        --eval_batch_size=32 \
        --max_seq_length=128 \
